bcb8df4826dd6a67f0919ab415f6912894830d23
dgiorgio <douglas.giorgio@gmail.com>
2019-06-13 21:26:32 -0300

    update: 20190612

--- dockerfile/src/gsa/deps/debian-deps.sh
+++ dockerfile/src/gsa/deps/debian-deps.sh
@@ -37,9 +37,10 @@ texlive-fonts-recommended
 apt update -y && \
 apt install -y --no-install-recommends --fix-missing ${BASE} ${DEPS} ${OPT}
 
-# nodejs
+# yarn
 curl --silent --show-error https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+# nodejs
 curl --silent --show-error https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 echo "deb https://deb.nodesource.com/node_8.x stretch main" | tee /etc/apt/sources.list.d/nodesource.list
 apt update -y

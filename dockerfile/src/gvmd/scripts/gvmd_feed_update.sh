16906c4030eabc2bd92a3fd042425419f76510c5
dgiorgio <douglas.giorgio@gmail.com>
2021-02-26 20:07:49 -0300

    script colors

--- dockerfile/src/gvmd/scripts/gvmd_feed_update.sh
+++ dockerfile/src/gvmd/scripts/gvmd_feed_update.sh
@@ -8,34 +8,34 @@ _PRINT() {
   COLOR='\033[0m'
   NC='\033[0m' # No Color
   [ "${1}" == "green" ] && COLOR='\033[0;32m'
+  [ "${1}" == "yellow" ] && COLOR='\033[0;33m'
   [ "${1}" == "red" ] && COLOR='\033[0;31m'
 
   echo -e "${COLOR}${MSG}${NC}"
 }
 
-
 _update_gvmd_data(){
-  echo "Update GVMD_DATA"
+  MSG="Update GVMD_DATA - starting!!!" _PRINT "yellow"
   greenbone-feed-sync --type GVMD_DATA
   feed_sync_gvmd_data="${?}"
-  [ "${feed_sync_gvmd_data}" == "0" ] && MSG="Update GVMD_DATA - success!!!" _PRINT "green" \
-  || MSG="Update GVMD_DATA - failed!!!" _PRINT "red"
+  [ "${feed_sync_gvmd_data}" == "0" ] && MSG="Update GVMD_DATA - success!!! \n" _PRINT "green" \
+  || MSG="Update GVMD_DATA - failed!!! \n" _PRINT "red"
 }
 
 _update_scap(){
-  echo "Update SCAP"
+  MSG="Update SCAP - starting!!!" _PRINT "yellow"
   greenbone-feed-sync --type SCAP
   feed_sync_scap="${?}"
-  [ "${feed_sync_scap}" == "0" ] && MSG="Update SCAP - success!!!" _PRINT "green" \
-  || MSG="Update SCAP - failed!!!" _PRINT "red"
+  [ "${feed_sync_scap}" == "0" ] && MSG="Update SCAP - success!!! \n" _PRINT "green" \
+  || MSG="Update SCAP - failed!!! \n" _PRINT "red"
 }
 
 _update_cert(){
-  echo "Update CERT"
+  MSG="Update CERT - starting!!!" _PRINT "yellow"
   greenbone-feed-sync --type CERT
   feed_sync_cert="${?}"
-  [ "${feed_sync_cert}" == "0" ] && MSG="Update CERT - success!!!" _PRINT "green" \
-  || MSG="Update CERT - failed!!!" _PRINT "red"
+  [ "${feed_sync_cert}" == "0" ] && MSG="Update CERT - success!!! \n" _PRINT "green" \
+  || MSG="Update CERT - failed!!! \n" _PRINT "red"
 }
 
 while [ "${feed_sync_gvmd_data}" != "0" ] || [ "${feed_sync_scap}" != "0" ] || [ "${feed_sync_cert}" != "0" ]; do
--- dockerfile/src/openvas-scanner/scripts/nvt_feed_update.sh
+++ dockerfile/src/openvas-scanner/scripts/nvt_feed_update.sh
@@ -5,15 +5,17 @@ _PRINT() {
   COLOR='\033[0m'
   NC='\033[0m' # No Color
   [ "${1}" == "green" ] && COLOR='\033[0;32m'
+  [ "${1}" == "yellow" ] && COLOR='\033[0;33m'
   [ "${1}" == "red" ] && COLOR='\033[0;31m'
 
   echo -e "${COLOR}${MSG}${NC}"
 }
 
 while [ "${feed_sync_nvt}" != "0" ]; do
+  MSG="Update NVT - starting!!!" _PRINT "yellow"
   /usr/local/bin/gvm-nvt-sync.sh
   feed_sync_nvt="${?}"
-  [ "${feed_sync_nvt}" == "0" ] && MSG="Update NVT - success!!!" _PRINT "green" \
-  || MSG="Update NVT - failed!!!" _PRINT "red"
+  [ "${feed_sync_nvt}" == "0" ] && MSG="\nUpdate NVT - success!!! \n" _PRINT "green" \
+  || MSG="\nUpdate NVTs - failed!!! \n" _PRINT "red"
   sleep 5
 done
